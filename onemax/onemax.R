1c7139aaa3b1f5cb2d3a37995a883384770f924c
DiasPhilipe <philipediasvintao@gmail.com>
2020-05-20 18:46:31 -0300

    Add files via upload

--- onemax/onemax.R
+++ onemax/onemax.R
@@ -2,25 +2,26 @@ library(ggplot2)
 library(data.table)
 library(dplyr)
 library(reticulate)
+library(ggcorrplot)
 
 ###
-py_run_file(file = "/MAI5030/onemax/onemax.py")
+py_run_file(file = "/media/philipe/5650A03850A020AB/onemax/onemax.py")
 
-data.table(Geracão = 1:100, cga = py$progress_cga, sga = py$progress_sga) %>%
+data.table(Geracão = 1:100, cga = unlist(py$progress_cga), sga = unlist(py$progress_sga)) %>%
   melt.data.table(id.vars = 'Geracão', variable.name = "Algorítimo", value.name = "Ajuste") %>%
   ggplot(aes(x = Geracão, y = Ajuste, color = Algorítimo)) + 
   geom_line() + 
   scale_color_brewer(palette = "Set1")
 
 ###
-py_run_file(file = "/MAI5030/onemax/onemax_comp.py")
+py_run_file(file = "/media/philipe/5650A03850A020AB/onemax/onemax_comp.py")
 
 for(i in 1:length(py$sizes)){
-  sga_acc = round(sum(py$fitness_sga[[i]]==py$global_fitness)/py$tests, digits = 3)*100
-  cga_acc = round(sum(py$fitness_cga[[i]]==py$global_fitness)/py$tests, digits = 3)*100
+  sga_acc = round(sum(unlist(py$fitness_sga[[i]])==py$global_fitness)/py$tests, digits = 3)*100
+  cga_acc = round(sum(unlist(py$fitness_cga[[i]])==py$global_fitness)/py$tests, digits = 3)*100
   
   print(
-    data.table(Execuções = 1:py$tests, cga = py$fitness_cga[[i]], sga = py$fitness_sga[[i]]) %>%
+    data.table(Execuções = 1:py$tests, cga = unlist(py$fitness_cga[[i]]), sga = unlist(py$fitness_sga[[i]])) %>%
       melt.data.table(id.vars = 'Execuções', variable.name = "Acurácia", value.name = "Ajuste") %>%
       mutate(Acurácia = if_else(Acurácia=="cga", paste0(Acurácia, " - ", cga_acc, "%"), paste0(Acurácia, " - ", sga_acc, "%"))) %>%
       ggplot(aes(x = Execuções, y = Ajuste, color = Acurácia)) + 
@@ -31,3 +32,47 @@ for(i in 1:length(py$sizes)){
     )
 }
 
+###
+py_run_file(file = "/media/philipe/5650A03850A020AB/onemax/onemax_cov.py")
+
+melt(py$sga_cm$icm, value.name = "Cov") %>%
+  ggplot(aes(x=Var1, y=Var2, fill=Cov)) +
+  geom_tile() +
+  ggtitle("sga - População inicial") +
+  theme(axis.title.x = element_blank(),
+        axis.title.y = element_blank())
+
+melt(py$sga_cm$tcm, value.name = "Cov") %>%
+  ggplot(aes(x=Var1, y=Var2, fill=Cov)) +
+  geom_tile() +
+  ggtitle("sga - População intermediária") +
+  theme(axis.title.x = element_blank(),
+        axis.title.y = element_blank())
+
+melt(py$sga_cm$fcm, value.name = "Cov") %>%
+  ggplot(aes(x=Var1, y=Var2, fill=Cov)) +
+  geom_tile() +
+  ggtitle("sga - População final") +
+  theme(axis.title.x = element_blank(),
+        axis.title.y = element_blank())
+
+melt(py$cga_cm$icm, value.name = "Cov") %>%
+  ggplot(aes(x=Var1, y=Var2, fill=Cov)) +
+  geom_tile() +
+  ggtitle("cga - População inicial") +
+  theme(axis.title.x = element_blank(),
+        axis.title.y = element_blank())
+
+melt(py$cga_cm$tcm, value.name = "Cov") %>%
+  ggplot(aes(x=Var1, y=Var2, fill=Cov)) +
+  geom_tile() +
+  ggtitle("cga - População intermediária") +
+  theme(axis.title.x = element_blank(),
+        axis.title.y = element_blank())
+
+melt(py$cga_cm$fcm, value.name = "Cov") %>%
+  ggplot(aes(x=Var1, y=Var2, fill=Cov)) +
+  geom_tile() +
+  ggtitle("cga - População final") +
+  theme(axis.title.x = element_blank(),
+        axis.title.y = element_blank())
