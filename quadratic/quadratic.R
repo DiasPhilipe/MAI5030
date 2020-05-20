bed3f5e7148a6056cb259132960a1cf15c9e17a5
DiasPhilipe <philipediasvintao@gmail.com>
2020-05-20 18:48:06 -0300

    Add files via upload

--- quadratic/quadratic.R
+++ quadratic/quadratic.R
@@ -4,7 +4,7 @@ library(dplyr)
 library(reticulate)
 
 ###
-py_run_file(file = "/MAI5030/quadratic/quadratic.py")
+py_run_file(file = "/media/philipe/5650A03850A020AB/quadratic/quadratic.py")
 
 data.table(Geracão = 1:100, cga = unlist(py$progress_cga), sga = unlist(py$progress_sga)) %>%
   melt.data.table(id.vars = 'Geracão', variable.name = "Algorítimo", value.name = "Ajuste") %>%
@@ -13,7 +13,7 @@ data.table(Geracão = 1:100, cga = unlist(py$progress_cga), sga = unlist(py$prog
   scale_color_brewer(palette = "Set1")
 
 ###
-py_run_file(file = "/MAI5030/quadratic/quadratic_comp.py")
+py_run_file(file = "/media/philipe/5650A03850A020AB/quadratic/quadratic_comp.py")
 
 for(i in 1:length(py$sizes)){
   sga_acc = round(sum(unlist(py$fitness_sga[[i]])==py$global_fitness)/py$tests, digits = 3)*100
@@ -31,3 +31,53 @@ for(i in 1:length(py$sizes)){
   )
 }
 
+###
+py_run_file(file = "/media/philipe/5650A03850A020AB/quadratic/quadratic_cov.py")
+
+melt(py$sga_cm$icm, value.name = "Cov") %>%
+  ggplot(aes(x=Var1, y=Var2, fill=Cov)) +
+  geom_tile() +
+  scale_fill_continuous(limits=c(-20, 20)) +
+  ggtitle("sga - População inicial") +
+  theme(axis.title.x = element_blank(),
+        axis.title.y = element_blank())
+
+melt(py$sga_cm$tcm, value.name = "Cov") %>%
+  ggplot(aes(x=Var1, y=Var2, fill=Cov)) +
+  geom_tile() +
+  scale_fill_continuous(limits=c(-20, 20)) +
+  ggtitle("sga - População intermediária") +
+  theme(axis.title.x = element_blank(),
+        axis.title.y = element_blank())
+
+melt(py$sga_cm$fcm, value.name = "Cov") %>%
+  ggplot(aes(x=Var1, y=Var2, fill=Cov)) +
+  geom_tile() +
+  scale_fill_continuous(limits=c(-20, 20)) +
+  ggtitle("sga - População final") +
+  theme(axis.title.x = element_blank(),
+        axis.title.y = element_blank())
+
+melt(py$cga_cm$icm, value.name = "Cov") %>%
+  ggplot(aes(x=Var1, y=Var2, fill=Cov)) +
+  geom_tile() +
+  scale_fill_continuous(limits=c(-20, 20)) +
+  ggtitle("cga - População inicial") +
+  theme(axis.title.x = element_blank(),
+        axis.title.y = element_blank())
+
+melt(py$cga_cm$tcm, value.name = "Cov") %>%
+  ggplot(aes(x=Var1, y=Var2, fill=Cov)) +
+  geom_tile() +
+  scale_fill_continuous(limits=c(-20, 20)) +
+  ggtitle("cga - População intermediária") +
+  theme(axis.title.x = element_blank(),
+        axis.title.y = element_blank())
+
+melt(py$cga_cm$fcm, value.name = "Cov") %>%
+  ggplot(aes(x=Var1, y=Var2, fill=Cov)) +
+  geom_tile() +
+  scale_fill_continuous(limits=c(-20, 20)) +
+  ggtitle("cga - População final") +
+  theme(axis.title.x = element_blank(),
+        axis.title.y = element_blank())
