e8895a4af259aac24ac7849d1ac9b7c8e01aeb1e
DiasPhilipe <philipediasvintao@gmail.com>
2020-05-20 14:50:23 -0300

    Add files via upload

---
+++ onemax/onemax.R
@@ -0,0 +1,33 @@
+library(ggplot2)
+library(data.table)
+library(dplyr)
+library(reticulate)
+
+###
+py_run_file(file = "/media/philipe/5650A03850A020AB/onemax.py")
+
+data.table(Geracão = 1:100, cga = py$progress_cga, sga = py$progress_sga) %>%
+  melt.data.table(id.vars = 'Geracão', variable.name = "Algorítimo", value.name = "Ajuste") %>%
+  ggplot(aes(x = Geracão, y = Ajuste, color = Algorítimo)) + 
+  geom_line() + 
+  scale_color_brewer(palette = "Set1")
+
+###
+py_run_file(file = "/media/philipe/5650A03850A020AB/onemax_comp.py")
+
+for(i in 1:length(py$sizes)){
+  sga_acc = round(sum(py$fitness_sga[[i]]==py$global_fitness)/py$tests, digits = 3)*100
+  cga_acc = round(sum(py$fitness_cga[[i]]==py$global_fitness)/py$tests, digits = 3)*100
+  
+  print(
+    data.table(Execuções = 1:py$tests, cga = py$fitness_cga[[i]], sga = py$fitness_sga[[i]]) %>%
+      melt.data.table(id.vars = 'Execuções', variable.name = "Acurácia", value.name = "Ajuste") %>%
+      mutate(Acurácia = if_else(Acurácia=="cga", paste0(Acurácia, " - ", cga_acc, "%"), paste0(Acurácia, " - ", sga_acc, "%"))) %>%
+      ggplot(aes(x = Execuções, y = Ajuste, color = Acurácia)) + 
+      ylim(c(0,20)) +
+      geom_line() + 
+      scale_color_brewer(palette = "Set1") +
+      ggtitle(paste0("População de tamanho ", py$sizes[i]))
+    )
+}
+
---
+++ onemax/onemax_comp.py
@@ -0,0 +1,121 @@
+################################################################################
+## Imports
+
+from random import random
+from pyeasyga import pyeasyga
+
+################################################################################
+## Common code
+
+# Population sizes
+sizes = [10, 20, 40, 80, 160]
+
+# Number of tests
+tests = 30
+
+# Fitness of the global solution
+global_fitness = 15
+
+# Data
+data = [0] * 15
+
+# Define fitness function
+def fitness_function(individual, data=None):
+  return sum(individual)
+  
+################################################################################
+## Simple Genetic Algorithm (sGA)
+
+# Best fitness of each execution
+fitness_sga = []
+
+for ps in sizes:
+  popfit = []
+  for _ in range(tests):
+    # Initialize genetic algorithm
+    sga = pyeasyga.GeneticAlgorithm(data, population_size=ps)
+    # Set fitness function
+    sga.fitness_function = fitness_function
+    # Run sGA
+    sga.run()
+    # Get best individual
+    fitness, result = sga.best_individual()
+    popfit.append(fitness)
+  fitness_sga.append(popfit)
+
+################################################################################
+## Compact Genetic Algorithm (cGA)
+
+# Best fitness of each execution
+fitness_cga = []
+
+# Update probability vector
+def update_prob(winner, loser, prob, popsize):
+  for i in range(0, len(prob)):
+    if winner[i] != loser[i]:
+      if winner[i] == 1:
+        prob[i] += 1.0 / float(popsize)
+      else:
+        prob[i] -= 1.0 / float(popsize)
+
+# Create a new individual
+def create_individual(prob):
+  individual = []
+  for p in prob:
+    if random() < p:
+      individual.append(1)
+    else:
+      individual.append(0)
+  return pyeasyga.Chromosome(individual)
+
+# Make competition between two individuals
+def compete(a, b):
+  if a.fitness > b.fitness:
+    return a, b
+  else:
+    return b, a
+
+# Set evolution function
+def run(self):
+  # Initialize probability vector
+  prob = [0.5] * len(self.seed_data)
+  # Initialize best solution
+  best = None
+  # Run `i` generations
+  for _ in range(0, self.generations):
+    # Create individuals
+    a = self.create_individual(prob)
+    b = self.create_individual(prob)
+    # Calculate fitness for each individual
+    a.fitness = self.fitness_function(a.genes)
+    b.fitness = self.fitness_function(b.genes)
+    # Get the best and worst individual
+    winner, loser = compete(a, b)
+    # Update best solution
+    if best:
+      if winner.fitness > best.fitness:
+        best = winner
+    else:
+      best = winner
+    # Update the probability vector based on the success of each bit
+    update_prob(winner.genes, loser.genes, prob, self.population_size)
+  # Add final solution
+  self.current_generation.append(best)
+
+for ps in sizes:
+  popfit = []
+  for _ in range(tests):
+    # Initialize genetic algorithm
+    cga = pyeasyga.GeneticAlgorithm(data, population_size=ps)
+    # Set create individual function
+    cga.create_individual = create_individual
+    # Set fitness function
+    cga.fitness_function = fitness_function
+    # Set evolution function
+    cga.run = run
+    # Run evolution
+    cga.run(cga)
+    # Get best individual
+    fitness, result = cga.best_individual()
+    popfit.append(fitness)
+  fitness_cga.append(popfit)
